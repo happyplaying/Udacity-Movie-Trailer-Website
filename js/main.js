0be32dbb01e48c6d04bbb957bd51941457d2ba53
Chao Guo <chao@Chaos-MacBook-Pro-Home.local>
2018-06-27 22:04:05 -0700

    add back to top

--- js/main.js
+++ js/main.js
@@ -16,3 +16,24 @@ $(document).on('click', '.movie-modal', function (event) {
         'frameborder': 0
     }));
 });
+// back to top
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $("#back-to-top").fadeIn();
+        } else {
+            $("#back-to-top").fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $("#back-to-top").click(function() {
+        $("#back-to-top").tooltip("hide");
+        $("body,html").animate(
+            {
+                scrollTop: 0
+            },
+            800
+        );
+        return false;
+    });
+});
